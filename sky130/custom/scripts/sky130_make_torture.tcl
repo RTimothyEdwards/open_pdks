d7289eba5e17fa4c0bc5618f994820b8901a437c
Tim Edwards <tim@opencircuitdesign.com>
2020-09-10 21:48:31 -0400

    Substantially revised the technology file for magic, the PDK device generator script for magic, the setup file for netgen, and the DRC torture test generator, to match the final naming conventions for the sky130_fd_pr library of primitive devices.

--- sky130/custom/scripts/sky130_make_torture.tcl
+++ sky130/custom/scripts/sky130_make_torture.tcl
@@ -62,7 +62,7 @@ proc mos_array {n devname startx starty {deltax 12700} {deltay 254}} {
 
 # NxN array resistor devices
 
-proc res_array {n devname startx starty {deltax 12700} {deltay 254}} {
+proc sky130_fd_pr__res_array {n devname startx starty {deltax 12700} {deltay 254}} {
    suspendall
    box position $startx $starty
    set i 0
@@ -87,9 +87,9 @@ proc res_array {n devname startx starty {deltax 12700} {deltay 254}} {
          if {[rand] > 0.5} {set gt 1} else {set gt 0}
          if {[rand] > 0.5} {set gb 1} else {set gb 0}
 
-	 # Snake geometry does not apply to xhrpoly, uhrpoly, and xpwres, and
+	 # Snake geometry does not apply to sky130_fd_pr_res_high_po, sky130_fd_pr__res_xhigh_po, and sky130_fd_pr__res_iso_pw, and
 	 # roverlap and endcov are prohibited.
-	 if {$devname == "xhrpoly" || $devname == "uhrpoly" || $devname == "xpwres"} {
+	 if {$devname == "sky130_fd_pr_res_high_po" || $devname == "res_xhigh_po" || $devname == "res_iso_pw"} {
 	    magic::gencell sky130::${devname} ${devname}_$i \
 		w $w l $l m $m nx $nx full_metal $fm \
 		glc $gl grc $gr gbc $gb gtc $gt
@@ -232,78 +232,75 @@ box size 0 0
  
 # Layout:
 #  phv
-#        mrl1     mrp1   mrdp   mrdp_hv  
-#  nhv                                  pdiode_h
-#                                       ndiode_h
-#  pshort                               nndiode
-#        xhrpoly  xpwres mrdn   mrdn_hv  pdiode
-#  nshort                               ndiode
+#        sky130_fd_pr__res_generic_l1     sky130_fd_pr__res_generic_po   res_generic_pd   res_generic_pd_hv  
+#  nfet_g5v0d10v5                                  diode_pd2nw_11v0
+#                                       diode_pw2nd_11v0
+#  pfet_01v8                               ndiode_pw2nd_05v5
+#       res_high_po  res_iso_pw res_generic_nd   res_generic_nd_hv  diode_pd2nw_05v5
+#  nfet_01v8                               diode_pw2nd_05v5
 #
 
-mos_array 6 nshort 0 0
-mos_array 6 pshort 0 75000
-mos_array 6 nhv 0 150000 
-mos_array 6 phv 0 225000
-mos_array 6 nlowvt 0 300000
-mos_array 6 sonos_e 0 375000 12700 1650
-mos_array 6 plowvt 0 450000
-mos_array 6 phighvt 0 525000
-mos_array 6 nhvnative 0 600000 
+mos_array 6 sky130_fd_pr__nfet_01v8 0 0
+mos_array 6 sky130_fd_pr__pfet_01v8 0 75000
+mos_array 6 sky130_fd_pr__nfet_g5v0d10v5 0 150000 
+mos_array 6 sky130_fd_pr__pfet_g5v0d10v5 0 225000
+mos_array 6 sky130_fd_pr__nfet_01v8_lvt 0 300000
+mos_array 6 sky130_fd_bs_flash__special_sonosfet_star 0 375000 12700 1650
+mos_array 6 sky130_fd_pr__pfet_01v8_lvt 0 450000
+mos_array 6 sky130_fd_pr__pfet_01v8_hvt 0 525000
+mos_array 6 sky130_fd_pr__nfet_03v3_nvt 0 600000 
 
-res_array 6 xhrpoly 100000 0
-res_array 6 mrl1 100000 180000
-res_array 6 uhrpoly 100000 360000
+res_array 6 sky130_fd_pr_res_high_po 100000 0
+res_array 6 sky130_fd_pr__res_generic_l1 100000 180000
+res_array 6 sky130_fd_pr__res_xhigh_po 100000 360000
 
-res_array 6 xpwres 200000 0 13500 1100
-res_array 6 mrp1 200000 200000
+res_array 6 sky130_fd_pr__res_iso_pw 200000 0 13500 1100
+res_array 6 sky130_fd_pr__res_generic_po 200000 200000
 
-res_array 6 mrdn 300000 0
-res_array 6 mrdp 300000 180000
+res_array 6 sky130_fd_pr__res_generic_nd 300000 0
+res_array 6 sky130_fd_pr__res_generic_pd 300000 180000
 
-res_array 6 mrdn_hv 400000 0
-res_array 6 mrdp_hv 400000 180000
- 
-diode_array 6 ndiode 500000 0
-diode_array 6 pdiode 500000 30000 
-diode_array 6 ndiode_h 500000 60000
-diode_array 6 pdiode_h 500000 90000 
+diode_array 6 sky130_fd_pr__diode_pw2nd_05v5 500000 0
+diode_array 6 sky130_fd_pr__diode_pd2nw_05v5 500000 30000 
+diode_array 6 sky130_fd_pr__diode_pw2nd_11v0 500000 60000
+diode_array 6 sky130_fd_pr__diode_pd2nw_11v0 500000 90000 
 
-cap_array 6 xcmimc1 600000 0
-cap_array 6 xcmimc2 600000 70000 18000 650
+cap_array 6 sky130_fd_pr__cap_mim_m3_1 600000 0
+cap_array 6 sky130_fd_pr__cap_mim_m3_2 600000 70000 18000 650
 
-mos_array 6 xcnwvc 700000 0 15000 254
-mos_array 6 xchvnwc 700000 70000 18000 254
-mos_array 6 xcnwvc2 700000 140000 15000 254
+mos_array 6 sky130_fd_pr__cap_var_lvt 700000 0 15000 254
+mos_array 6 sky130_fd_pr__cap_var 700000 70000 18000 254
+mos_array 6 sky130_fd_pr__cap_var_hvt 700000 140000 15000 254
 
-fixed_array 2 sky130_fd_pr_rf_npn_1x1 850000 0
-fixed_array 2 sky130_fd_pr_rf_npn_1x2 850000 30000
-fixed_array 2 sky130_fd_pr_rf_pnp5x 850000 60000
+fixed_array 2 sky130_fd_pr__rf_npn_1x1 850000 0
+fixed_array 2 sky130_fd_pr__rf_npn_1x2 850000 30000
+fixed_array 2 sky130_fd_pr__rf_pnp5x 850000 60000
 
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp11p5x11p7_lim5shield 900000 0
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp11p5x11p7_m3_lim5shield 900000 30000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp11p5x11p7_m4shield 900000 60000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp11p5x11p7_polym4shield 900000 90000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp4p4x4p6_m3_lim5shield 900000 120000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp6p8x6p1_lim4shield 900000 150000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp6p8x6p1_polym4shield 900000 180000
-fixed_array 2 sky130_fd_pr_rf2_xcmvpp8p6x7p9_m3_lim5shield 900000 210000
-fixed_array 2 sky130_fd_pr_rf2_xcmvppx4_2xnhvnative10x4 900000 240000
-# fixed_array 2 sky130_fd_pr_rf2_xcmvpp11p5x11p7_polym50p4shield 900000 270000
+fixed_array 2 sky130_fd_pr__cap_vpp_01p8x01p8_m1m2_noshield 900000 0
+fixed_array 2 sky130_fd_pr__cap_vpp_02p4x04p6_m1m2_noshield 900000 30000
+fixed_array 2 sky130_fd_pr__cap_vpp_02p7x6p1_m1m2m3m4_shield1_fingercap 900000 60000
+fixed_array 2 sky130_fd_pr__cap_vpp_01p8x01p8_m1m2_shield1 900000 90000
+fixed_array 2 sky130_fd_pr__cap_vpp_03p9x03p9_m1m2_shield1_floatm3 900000 120000
+fixed_array 2 sky130_fd_pr__cap_vpp_04p4x04p6_l1m1m2_noshield 900000 150000
+fixed_array 2 sky130_fd_pr__cap_vpp_04p4x04p6_l1m1m2_shieldm3_floatpo 900000 180000
+fixed_array 2 sky130_fd_pr__cap_vpp_04p4x04p6_l1m1m2m3m4_shieldpom5 900000 210000
+fixed_array 2 sky130_fd_pr__cap_vpp_06p8x06p1_l1m1m2_noshield 900000 240000
+fixed_array 2 sky130_fd_pr__cap_vpp_06p8x06p1_l1m1m2m3m4_shieldpo_floatm5 900000 270000
 
-fixed_array 2 sky130_fd_pr_rf_xcmvpp11p5x11p7_m3_lishield 950000 0
-fixed_array 2 sky130_fd_pr_rf_xcmvpp11p5x11p7_m3shield 950000 30000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp2 950000 60000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp2_nwell 950000 90000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp4p4x4p6_m3_lishield 950000 120000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp4p4x4p6_m3shield 950000 150000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp8p6x7p9_m3_lishield 950000 180000
-fixed_array 2 sky130_fd_pr_rf_xcmvpp8p6x7p9_m3shield 950000 210000
-# fixed_array 2 sky130_fd_pr_rf_xcmvpp1p8x1p8_lishield 950000 240000
-# fixed_array 2 sky130_fd_pr_rf_xcmvpp1p8x1p8_m3shield 950000 270000
+fixed_array 2 sky130_fd_pr__cap_vpp_08p6x07p8_l1m1m2_noshield 950000 0
+fixed_array 2 sky130_fd_pr__cap_vpp_08p6x07p8_l1m1m2_shieldnw_nwell 950000 30000
+fixed_array 2 sky130_fd_pr__cap_vpp_08p6x07p8_m1m2m3_shield1 950000 60000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p3x11p3_m1m2m3m4_shield1_wafflecap 950000 90000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p3x11p8_l1m1m2m3m4_shield5_nhv 950000 120000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p5x11p7_l1m1m2_noshield 950000 150000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p5x11p7_l1m1m2_shieldpom3 950000 180000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p5x11p7_l1m1m2m3m4_shieldm5 950000 210000
+fixed_array 2 sky130_fd_pr__cap_vpp_11p5x11p7_m1m4_noshield 950000 240000
+fixed_array 2 sky130_fd_pr__cap_vpp_22p5x11p7_pol1m1m2m3m4m5_noshield 950000 270000
 
-fixed_array 1 balun 1000000 0
-fixed_array 1 xind4_02 1000000 200000
-fixed_array 1 xind4_011 1250000 0
+# fixed_array 1 balun 1000000 0
+fixed_array 1 sky130_fd_pr__ind_02_04 1000000 200000
+fixed_array 1 sky130_fd_pr__ind_11_04 1250000 0
 
 # Draw a deep nwell region around the sonos transistor block
 box values -5200i 369500i 76800i 427500i
