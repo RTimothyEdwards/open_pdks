6f4d30491fafe5a6c1985a4ad6410818833f4506
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-09-26 00:46:59 +0200

    openlane support for latch mapping in synthesis

---
+++ sky130/openlane/sky130_fd_sc_ms/latch_map.v
@@ -0,0 +1,21 @@
+module \$_DLATCH_P_ (input E, input D, output Q);
+  sky130_fd_sc_ms__dlxtp_1 _TECHMAP_DLATCH_P (
+    //# {{data|Data Signals}}
+    .D(D),
+    .Q(Q),
+
+    //# {{clocks|Clocking}}
+    .GATE(E)
+  );
+endmodule
+
+module \$_DLATCH_N_ (input E, input D, output Q);
+  sky130_fd_sc_ms__dlxtn_1 _TECHMAP_DLATCH_N (
+    //# {{data|Data Signals}}
+    .D(D),
+    .Q(Q),
+
+    //# {{clocks|Clocking}}
+    .GATE_N(E)
+  );
+endmodule
