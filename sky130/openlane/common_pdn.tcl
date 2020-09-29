37378d1ce921b5b4cef0388bd46f02dcfe65c683
agorararmard <aagouhar@efabless.com>
2020-09-29 21:18:09 +0200

    core ring control in openlane configs

--- sky130/openlane/common_pdn.tcl
+++ sky130/openlane/common_pdn.tcl
@@ -4,7 +4,7 @@ set ::ground_nets $::env(GND_PIN)
 
 set ::macro_blockage_layer_list "li1 met1 met2 met3 met4 met5"
 
-pdngen::specify_grid stdcell {
+set stdcell {
     name grid
     rails {
 	    met1 {width $::env(FP_PDN_RAIL_WIDTH) pitch $::env(PLACE_SITE_HEIGHT) offset $::env(FP_PDN_RAIL_OFFSET)}
@@ -15,6 +15,18 @@ pdngen::specify_grid stdcell {
     }
     connect {{met1 met4} {met4 met5}}
 }
+
+if { [info exists ::env(FP_PDN_CORE_RING)] } {
+    if { $::env(FP_PDN_CORE_RING) == 1 } {
+        dict append stdcell core_ring {
+                met4 {width $::env(FP_PDN_CORE_RING_VWIDTH) spacing $::env(FP_PDN_CORE_RING_VSPACING) core_offset $::env(FP_PDN_CORE_RING_VOFFSET)}
+                met5 {width $::env(FP_PDN_CORE_RING_HWIDTH) spacing $::env(FP_PDN_CORE_RING_HSPACING) core_offset $::env(FP_PDN_CORE_RING_HOFFSET)}
+            }
+    }
+}
+
+pdngen::specify_grid stdcell $stdcell
+
 
 pdngen::specify_grid macro {
     orient {R0 R180 MX MY R90 R270 MXR90 MYR90}
