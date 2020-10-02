b1a18424686ecb7467692a0c63d8b0f41493a4f7
Tim Edwards <tim@opencircuitdesign.com>
2020-10-02 08:51:29 -0400

    Corrected capacitor extraction (needed type "csubcircuit" and not "subcircuit" to get the correct L and W calculation).

--- sky130/openlane/common_pdn.tcl
+++ sky130/openlane/common_pdn.tcl
@@ -7,7 +7,7 @@ set ::macro_blockage_layer_list "li1 met1 met2 met3 met4 met5"
 pdngen::specify_grid stdcell {
     name grid
     rails {
-	    met1 {width $::env(FP_PDN_RAIL_WIDTH) pitch $::env(PLACE_SITE_HEIGHT) offset $::env(FP_PDN_RAIL_OFFSET)}
+	    met1 {width $::env(PDN_RAIL_WIDTH) pitch $::env(PLACE_SITE_HEIGHT) offset 0}
     }
     straps {
 	    met4 {width $::env(FP_PDN_VWIDTH) pitch $::env(FP_PDN_VPITCH) offset $::env(FP_PDN_VOFFSET)}
