a5854315ed3b1198d840aef8acda0004ec624354
Tristan Gingold <tgingold@free.fr>
2020-10-15 18:28:16 +0200

    sky130: add support for sky130_osu_sc_t18
    Initial support for the t18 variant of OSU sky130.
    
    Good enough to build the spm design from OpenLane, but requires a few
    fixes in sky130_osu_sc_t18 and in OpenRoad.

---
+++ sky130/openlane/sky130_osu_sc_t18/config.tcl
@@ -0,0 +1,73 @@
+set current_folder [file dirname [file normalize [info script]]]
+# Technology lib
+
+#ifdef EF_FORMAT
+set ::env(LIB_SYNTH) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/lib/$::env(STD_CELL_LIBRARY)/sky130_osu_sc_TT_1P8_25C.lib"
+set ::env(LIB_FASTEST) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/lib/$::env(STD_CELL_LIBRARY)/sky130_osu_sc_FF_1P8_25C.lib"
+set ::env(LIB_SLOWEST) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/lib/$::env(STD_CELL_LIBRARY)/sky130_osu_sc_SS_1p8_25C.lib"
+#else (!EF_FORMAT)
+set ::env(LIB_SYNTH) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/$::env(STD_CELL_LIBRARY)/lib/sky130_osu_sc_TT_1P8_25C.lib"
+set ::env(LIB_FASTEST) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/$::env(STD_CELL_LIBRARY)/lib/sky130_osu_sc_FF_1P8_25C.lib"
+set ::env(LIB_SLOWEST) "$::env(PDK_ROOT)/$::env(PDK)/libs.ref/$::env(STD_CELL_LIBRARY)/lib/sky130_osu_sc_SS_1P8_25C.lib"
+#endif (!EF_FORMAT)
+
+set ::env(LIB_TYPICAL) $::env(LIB_SYNTH)
+
+# Placement site for core cells
+# This can be found in the technology lef (SITE)
+set ::env(PLACE_SITE) "s8_osu130"
+set ::env(PLACE_SITE_WIDTH) 0.11
+set ::env(PLACE_SITE_HEIGHT) 7.2
+
+# welltap and endcap cells
+# Does not exist in OSU cells.  Taps are included in each cell.
+# set ::env(FP_WELLTAP_CELL) "sky130_osu_sc_t18__tapvpwrvgnd_1"
+# set ::env(FP_ENDCAP_CELL) "sky130_osu_sc_t18__decap_3"
+
+# defaults (can be overridden by designs):
+set ::env(SYNTH_DRIVING_CELL) "INVX8"
+set ::env(SYNTH_DRIVING_CELL_PIN) "Y"
+# capacitance : 0.037357;
+# update these
+set ::env(SYNTH_CAP_LOAD) "37.357" ; # femtofarad INVX8 pin A cap
+set ::env(SYNTH_MIN_BUF_PORT) "BUFX1 A Y"
+set ::env(SYNTH_TIEHI_PORT) "TIEHI Y"
+set ::env(SYNTH_TIELO_PORT) "TIELO Y"
+
+# Power pins
+set ::env(VDD_PIN) "vdd"
+set ::env(GND_PIN) "gnd"
+
+# cts defaults
+set ::env(CTS_ROOT_BUFFER) CLKBUFX1
+set ::env(CELL_CLK_PORT) CK
+
+# Placement defaults
+set ::env(PL_LIB) $::env(LIB_TYPICAL)
+
+# Fillcell insertion - do decap cell
+set ::env(FILL_CELL) "FILLX"
+set ::env(DECAP_CELL) ""
+set ::env(RE_BUFFER_CELL) "BUFX4"
+
+
+# Diode insertion
+set ::env(DIODE_CELL) "ANTFILL"
+#set ::env(FAKEDIODE_CELL) "sky130_osu_sc_t18__fakediode_2"
+set ::env(DIODE_CELL_PIN) "A"
+
+set ::env(CELL_PAD) 8
+set ::env(CELL_PAD_EXECLUDE) "FILLX*"
+
+# Clk Buffers info CTS data
+set ::env(ROOT_CLK_BUFFER) CLKBUFX1
+set ::env(CLK_BUFFER) CLKBUFX1
+set ::env(CLK_BUFFER_INPUT) A
+set ::env(CLK_BUFFER_OUTPUT) Y
+set ::env(CTS_CLK_BUFFER_LIST) "CLKBUFX1"
+# TODO...
+set ::env(CTS_SQR_CAP) 0.258e-3
+set ::env(CTS_SQR_RES) 0.125
+set ::env(CTS_MAX_CAP) 1.53169
+
+set ::env(FP_PDN_RAIL_WIDTH) 0.48
--- sky130/qflow/sky130osu.sh
+++ sky130/qflow/sky130osu.sh
@@ -9,21 +9,21 @@
 #ifdef EF_FORMAT
 set leffile=STAGING_PATH/TECHNAME/libs.ref/lef/sky130_osu130/sky130_osu130.lef
 #else (!EF_FORMAT)
-set leffile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/lef/sky130_osu130.lef
+set leffile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/lef/sky130_osu130.lef
 #endif (!EF_FORMAT)
 
 # The SPICE netlist containing subcell definitions for all the standard cells
 #ifdef EF_FORMAT
 set spicefile=STAGING_PATH/TECHNAME/libs.ref/spice/sky130_osu130/sky130_osu130.spice
 #else (!EF_FORMAT)
-set spicefile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/spice/sky130_osu130.spice
+set spicefile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/spice/sky130_osu130.spice
 #endif (!EF_FORMAT)
 
 # The liberty format file containing standard cell timing and function information
 #ifdef EF_FORMAT
 set libertyfile=STAGING_PATH/TECHNAME/libs.ref/lib/sky130_osu130/sky130_osu130.lib
 #else (!EF_FORMAT)
-set libertyfile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/lib/sky130_osu130.lib
+set libertyfile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/lib/sky130_osu130.lib
 #endif (!EF_FORMAT)
 
 # If there is another LEF file containing technology information
@@ -34,7 +34,7 @@ set libertyfile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/lib/sky130_osu130.l
 #ifdef EF_FORMAT
 set techleffile=STAGING_PATH/TECHNAME/libs.ref/techLEF/sky130_osu130/sky130_osu130_tech.lef
 #else (!EF_FORMAT)
-set techleffile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/techLEF/sky130_osu130_tech.lef
+set techleffile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/techLEF/sky130_osu130_tech.lef
 #endif (!EF_FORMAT)
 #else
 # NOTE:  There is no technology LEF file for the 3-metal stack!
@@ -52,16 +52,16 @@ set clkbufpin_in=A		;# Name of input port to buffer cell
 set clkbufpin_out=Y		;# Name of output port to buffer cell
 
 set fillcell=FILL		;# Spacer (filler) cell (prefix, if more than one)
-set decapcell=""		;# Decap (filler) cell (prefix, if more than one)
-set antennacell=""		;# Antenna (filler) cell (prefix, if more than one)
-set antennapin_in=""		;# Antenna cell input connection
+set decapcell="DECAP"		;# Decap (filler) cell (prefix, if more than one)
+set antennacell="ANT"		;# Antenna (filler) cell (prefix, if more than one)
+set antennapin_in="A"		;# Antenna cell input connection
 set bodytiecell=""		;# Body tie (filler) cell (prefix, if more than one)
 
 # yosys tries to eliminate use of these; depends on source .v
-set tiehi=""			;# Cell to connect to power, if one exists
-set tiehipin_out=""		;# Output pin name of tiehi cell, if it exists
-set tielo=""			;# Cell to connect to ground, if one exists
-set tielopin_out=""		;# Output pin name of tielo cell, if it exists
+set tiehi="TIEHI"		;# Cell to connect to power, if one exists
+set tiehipin_out="Y"		;# Output pin name of tiehi cell, if it exists
+set tielo="TIELO"		;# Cell to connect to ground, if one exists
+set tielopin_out="Y"		;# Output pin name of tielo cell, if it exists
 
 set gndnet="vdd"		;# Name used for ground pins in standard cells
 set vddnet="vss"		;# Name used for power pins in standard cells
@@ -72,11 +72,11 @@ set magicrc=STAGING_PATH/TECHNAME/MAGIC_CURRENT/TECHNAME.magicrc    ;# magic sta
 set magic_display="XR" 	;# magic display, defeat display query and OGL preference
 set netgen_setup=STAGING_PATH/TECHNAME/libs.tech/netgen/TECHNAME_setup.tcl	;# netgen setup file for LVS
 #ifdef EF_FORMAT
-set gdsfile=STAGING_PATH/TECHNAME/libs.ref/gds/sky130_osu130/sky130_osu130.gds	;# GDS database of standard cells
-set verilogfile=STAGING_PATH/TECHNAME/libs.ref/verilog/sky130_osu130/sky130_osu130.v	;# Verilog models of standard cells
+set gdsfile=STAGING_PATH/TECHNAME/libs.ref/gds/LIBRARY/sky130_osu130.gds	;# GDS database of standard cells
+set verilogfile=STAGING_PATH/TECHNAME/libs.ref/verilog/LIBRARY/sky130_osu130.v	;# Verilog models of standard cells
 #else (!EF_FORMAT)
-set gdsfile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/gds/sky130_osu130.gds	;# GDS database of standard cells
-set verilogfile=STAGING_PATH/TECHNAME/libs.ref/sky130_osu130/verilog/sky130_osu130.v	;# Verilog models of standard cells
+set gdsfile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/gds/sky130_osu130.gds	;# GDS database of standard cells
+set verilogfile=STAGING_PATH/TECHNAME/libs.ref/LIBRARY/verilog/sky130_osu130.v	;# Verilog models of standard cells
 #endif (!EF_FORMAT)
 
 # Set a conditional default in the project_vars.sh file for this process
