c1302c25f0cb571c67a84847d585cb4788dac6a1
agorararmard <aagouhar@efabless.com>
2020-10-21 19:39:39 +0200

    base commit for travisCI auto testing

---
+++ .travisCI/travisBuild.sh
@@ -0,0 +1,31 @@
+# Copyright 2020 Efabless Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# exit when any command fails
+set -e
+
+mkdir pdks
+export OPEN_PDKS_ROOT=$(pwd)
+export PDK_ROOT=$(pwd)/pdks
+cd ./.travisCI
+sh ./build-docker.sh > /dev/null
+make skywater-pdk > /dev/null
+if [ $STD_CELL_LIBRARY == all ]; then
+    make all-skywater-libraries;
+else
+    make skywater-library;
+fi
+cd ..
+docker run -it -v $(pwd):/some_root -v $(pwd)/.travisCI:/build_root -v $OPEN_PDKS_ROOT:$OPEN_PDKS_ROOT -v $PDK_ROOT:$PDK_ROOT -e OPEN_PDKS_ROOT=$OPEN_PDKS_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) magic:latest  bash -c "cd /build_root && make build-pdk"
+exit 0
