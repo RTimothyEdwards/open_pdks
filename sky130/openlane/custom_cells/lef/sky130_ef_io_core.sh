12131893c5b3891ffbadf717466d0cef0d090da7
Ahmed Ghazy <ax3ghazy@aucegypt.edu>
2020-12-29 22:58:40 +0200

    Update routing version of the IOs
    - the includes recent updates like accurate obstructions outside the
      abutment bounding box, filling met1 holes, remove met5 notches, etc.

--- sky130/openlane/custom_cells/lef/sky130_ef_io_core.sh
+++ sky130/openlane/custom_cells/lef/sky130_ef_io_core.sh
@@ -1,5 +1,5 @@
 $OPENLANE_ROOT/scripts/rectify_above.py -1.5 \
-	sky130_ef_io__gpiov2_pad < sky130_ef_io.lef |
+	sky130_ef_io__gpiov2_pad < ../../../../libs.ref/sky130_fd_io/lef/sky130_ef_io.lef |
 	$OPENLANE_ROOT/scripts/obs_above.py -1.35 0.16 0.16 \
 	sky130_ef_io__gpiov2_pad > sky130_ef_io.obs.gpiov2.lef.tmp
 
@@ -19,30 +19,48 @@ $OPENLANE_ROOT/scripts/rectify_above.py -1.5 \
 $OPENLANE_ROOT/scripts/rectify_above.py 0 \
 	sky130_ef_io__vccd_hvc_pad \
 	sky130_ef_io__vdda_hvc_pad \
+	sky130_ef_io__vdda_hvc_clamped_pad \
 	sky130_ef_io__vssa_hvc_pad \
+	sky130_ef_io__vssa_hvc_clamped_pad \
 	sky130_ef_io__vssd_hvc_pad \
 	sky130_ef_io__vddio_hvc_pad \
-	sky130_ef_io__vssio_hvc_pad < sky130_ef_io.obs.gpiov2.lef.tmp |
+	sky130_ef_io__vddio_hvc_clamped_pad \
+	sky130_ef_io__vssio_hvc_pad \
+	sky130_ef_io__vssio_hvc_clamped_pad \
+	< sky130_ef_io.obs.gpiov2.lef.tmp |
 	$OPENLANE_ROOT/scripts/obs_above.py 0 0 0 \
 	sky130_ef_io__vccd_hvc_pad \
 	sky130_ef_io__vdda_hvc_pad \
+	sky130_ef_io__vdda_hvc_clamped_pad \
 	sky130_ef_io__vssa_hvc_pad \
+	sky130_ef_io__vssa_hvc_clamped_pad \
 	sky130_ef_io__vssd_hvc_pad \
 	sky130_ef_io__vddio_hvc_pad \
-	sky130_ef_io__vssio_hvc_pad > sky130_ef_io.rect.obs.hvc.lef.tmp
+	sky130_ef_io__vddio_hvc_clamped_pad \
+	sky130_ef_io__vssio_hvc_pad \
+	sky130_ef_io__vssio_hvc_clamped_pad \
+	> sky130_ef_io.rect.obs.hvc.lef.tmp
 
 $OPENLANE_ROOT/scripts/rectify_above.py 1.4 \
 	sky130_ef_io__vccd_lvc_pad \
+	sky130_ef_io__vccd_lvc_clamped_pad \
+	sky130_ef_io__vccd_lvc_clamped2_pad \
 	sky130_ef_io__vdda_lvc_pad \
 	sky130_ef_io__vssa_lvc_pad \
 	sky130_ef_io__vssd_lvc_pad \
+	sky130_ef_io__vssd_lvc_clamped_pad \
+	sky130_ef_io__vssd_lvc_clamped2_pad \
 	sky130_ef_io__vddio_lvc_pad \
 	sky130_ef_io__vssio_lvc_pad < sky130_ef_io.rect.obs.hvc.lef.tmp |
 	$OPENLANE_ROOT/scripts/obs_above.py 1.4 0 0 \
 	sky130_ef_io__vccd_lvc_pad \
+	sky130_ef_io__vccd_lvc_clamped_pad \
+	sky130_ef_io__vccd_lvc_clamped2_pad \
 	sky130_ef_io__vdda_lvc_pad \
 	sky130_ef_io__vssa_lvc_pad \
 	sky130_ef_io__vssd_lvc_pad \
+	sky130_ef_io__vssd_lvc_clamped_pad \
+	sky130_ef_io__vssd_lvc_clamped2_pad \
 	sky130_ef_io__vddio_lvc_pad \
 	sky130_ef_io__vssio_lvc_pad > sky130_ef_io.rect.obs.lvc.lef.tmp
 
--- sky130/openlane/custom_cells/lef/sky130_fd_io_core.sh
+++ sky130/openlane/custom_cells/lef/sky130_fd_io_core.sh
@@ -1,4 +1,4 @@
-$OPENLANE_ROOT/scripts/rectify_above.py 1.635 sky130_fd_io__top_xres4v2 < sky130_fd_io.lef |
+$OPENLANE_ROOT/scripts/rectify_above.py 1.635 sky130_fd_io__top_xres4v2 < ../../../../libs.ref/sky130_fd_io/lef/sky130_fd_io.lef |
 	$OPENLANE_ROOT/scripts/obs_above.py 1.635 0 0 sky130_fd_io__top_xres4v2 > sky130_fd_io_core.lef.tmp
 
 # clean up
