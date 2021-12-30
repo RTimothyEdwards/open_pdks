9b4b10c03e58ff23f10a3dfd8f1fbba61df9fe70
Tim Edwards <tim@opencircuitdesign.com>
2021-12-29 20:22:15 -0500

    Modified the sp_to_spice.py script so that it removes the filename with the original extension after filtering.

--- sky130/custom/scripts/sp_to_spice.py
+++ sky130/custom/scripts/sp_to_spice.py
@@ -4,7 +4,11 @@
 #
 # This script runs as a filter to foundry_install.sh and converts file
 # names ending with ".sp" to ".spice".  If the file has multiple extensions
-# then all are stripped before adding ".spice".
+# then all are stripped before adding ".spice".  The script is specifically
+# intended to run on the SRAM macro library ".lvs.sp" files, which include
+# two parasitic devices that make them compatible with the extracted layout,
+# but have a few syntactical incompatibilities that can be corrected by
+# filtering the file during the copy.
 #
 # This script is a filter to be run by setting the name of this script as
 # the value to "filter=" for the model install in the sky130 Makefile.
@@ -88,6 +92,9 @@ def filter(inname):
             print('sp_to_spice.py: failed to open ' + outname + ' for writing.', file=sys.stderr)
             return 1
 
+        # If the name was changed, remove the original file.
+        if filename != newname:
+            os.remove(inname)
 
 if __name__ == '__main__':
 
