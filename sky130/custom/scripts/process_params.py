477d9b79b5579b91bf47d9ed68a8271a3bfcace7
Tim Edwards <tim@opencircuitdesign.com>
2022-02-06 17:01:15 -0500

    Modified a number of sky130 custom scripts that were all based on the same code and all generated a file called "temp" during processing, which was causing issues with files getting mixed up when using multithreading (note that the previous commit, which was an attempt to resolve the file mixup error, was a total red herring and had nothing to do with the issue).  All of the scripts now append "_temp" to the root name of the original input file being modified, instead of always naming a file "temp".  This should resolve the file mixup issue completely.

--- sky130/custom/scripts/process_params.py
+++ sky130/custom/scripts/process_params.py
@@ -84,7 +84,9 @@ print('')
 for infile_name in filelist:
 
     filepath = os.path.split(infile_name)[0]
-    outfile_name = os.path.join(filepath, 'temp')
+    filename = os.path.split(infile_name)[1]
+    fileroot = os.path.splitext(filename)[0]
+    outfile_name = os.path.join(filepath, fileroot + '_temp')
 
     infile = open(infile_name, 'r')
     outfile = open(outfile_name, 'w')
