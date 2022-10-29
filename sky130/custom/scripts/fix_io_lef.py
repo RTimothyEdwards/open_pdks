002b5dd29d81e9affec60cb6d57ed867c888e23c
Tim Edwards <tim@opencircuitdesign.com>
2022-10-29 17:55:04 -0400

    One more pass---since magic had difficulty correctly extracting the "C" shaped resistors cutting VDDIO from VDDIO_Q and VSSIO from VSSIO_Q, I added one more resistor to make them a ring, and now the resistors extract with the correct (effective) L and W.  Created a LEF view for top_power_hvc_wpadv2 which previously did not exist in the PDK, and created a script to modify the LEF views of the cells for which the resistor ring was added, so that the port annotation does not overwrite the resistors.  I now have LVS scripts running on 12 pad types and they all come out clean, including properties and pins.

---
+++ sky130/custom/scripts/fix_io_lef.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python3
+#
+# fix_io_lef ---
+#
+# This script fixes the issue where the LEF files of the I/O cells, which are
+# used for annotation only, interfere with the modified layouts.  The fix is
+# to remove all G_CORE port geometry that is above Y=5
+
+import re
+import os
+import sys
+
+def filter(inname, outname):
+
+    # Read input
+    try:
+        with open(inname, 'r') as inFile:
+            ltext = inFile.read()
+            llines = ltext.splitlines()
+    except:
+        print('fix_io_lef.py: failed to open ' + inname + ' for reading.', file=sys.stderr)
+        return 1
+
+    # Process input with regexp
+
+    fixedlines = []
+    modified = False
+    inpin = False
+
+    macrorex = re.compile('^MACRO[ \t]+([^ \t\n]+)')
+    pinrex = re.compile('^[ \t]*PIN[ \t]+[PG]_CORE')
+    endrex = re.compile('[ \t]*END[ \t]+[PG]_CORE')
+    rectrex = re.compile('^[ \t]*RECT[ \t]+[0-9.]+[ \t]+([0-9.]+)[ \t]+')
+    rect2rex = re.compile('^[ \t]*RECT[ \t]+[0-9.]+[ \t]+([0-9.]+)[ \t]+[0-9.]+[ \t]+([0-9.]+)[ \t]+')
+    macroname = None
+
+    for line in llines:
+
+        # Check for MACRO line and record the macro name
+        mmatch = macrorex.match(line)
+        if mmatch:
+            macroname = mmatch.group(1)
+
+        # Check for "PIN G_CORE" or "PIN P_CORE"
+        pmatch = pinrex.match(line)
+        if pmatch and macroname:
+            inpin = True
+
+        ematch = endrex.match(line)
+        if ematch and macroname:
+            inpin = False
+
+        if inpin:
+            # Check for RECT entries and get the lower Y value
+            rmatch = rectrex.match(line)
+            if rmatch:
+                yval = float(rmatch.group(1))
+                if yval > 5.0:
+                    # Reject geometry with lower Y value greater than 5um
+                    continue
+                # Truncate the height of any box with Y at zero that is
+                # higher than 50um 
+                elif yval < 1.0:
+                    rmatch = rect2rex.match(line)
+                    if rmatch:
+                        ytop = float(rmatch.group(2))
+                        if ytop > 50.0:
+                            line = line.replace(rmatch.group(2), '50.000')
+
+        fixedlines.append(line)
+
+    # Write output
+    if outname == None:
+        for i in fixedlines:
+            print(i)
+    else:
+        # If the output is a symbolic link but no modifications have been made,
+        # then leave it alone.  If it was modified, then remove the symbolic
+        # link before writing.
+        if os.path.islink(outname):
+            if not modified:
+                return 0
+            else:
+                os.unlink(outname)
+        try:
+            with open(outname, 'w') as outFile:
+                for i in fixedlines:
+                    print(i, file=outFile)
+        except:
+            print('fix_io_lef.py: failed to open ' + outname + ' for writing.', file=sys.stderr)
+            return 1
+
+
+if __name__ == '__main__':
+
+    # This script expects to get one or two arguments.  One argument is
+    # mandatory and is the input file.  The other argument is optional and
+    # is the output file.  The output file and input file may be the same
+    # name, in which case the original input is overwritten.
+
+    options = []
+    arguments = []
+    for item in sys.argv[1:]:
+        if item.find('-', 0) == 0:
+            options.append(item[1:])
+        else:
+            arguments.append(item)
+
+    if len(arguments) > 0:
+        infilename = arguments[0]
+
+    if len(arguments) > 1:
+        outfilename = arguments[1]
+    else:
+        outfilename = None
+
+    result = filter(infilename, outfilename)
+    sys.exit(result)
---
+++ sky130/custom/scripts/fix_vssio_overlay_gds.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+#
+# fix_vssio_overlay_gds.py ---
+#
+# Special-purpose script that does the work of what ought to be a simple
+# binary diff and patch.  Except that no such thing exists as a standard
+# offering on most Linux systems, so instead of adding another OS
+# package requirement, I'm just writing a binary search-and-replace in
+# python.
+#
+# The purpose of the patch is to narrow down the metal 3 area under
+# the vias in this cell to clear the metal3 resistor marker that has
+# been added to the base cell.  Since the overlay just duplicates the
+# metal 3 layer, this has no impact on the physical mask geometry.
+#
+
+import sys
+
+if len(sys.argv) != 2:
+    print('Usage:  fix_vssio_overlay_gds.py <filename>')
+    sys.exit(1)
+else:
+    file_name = sys.argv[1]
+
+# Convert this data
+orig_data_0 = b'\x00\x00\x01\xef\x00\x00\xe3\x80\x00\x00\x01\xef\x00\x00\xf4\xd8\x00\x00\x5f\x4b\x00\x00\xf4\xd8\x00\x00\x5f\x4b\x00\x00\xe3\x80\x00\x00\x01\xef'
+
+# To this data (move X position 24395 to 24370), (move X position 495 to 520)
+new_data_0 = b'\x00\x00\x02\x08\x00\x00\xe3\x80\x00\x00\x02\x08\x00\x00\xf4\xd8\x00\x00\x5f\x32\x00\x00\xf4\xd8\x00\x00\x5f\x32\x00\x00\xe3\x80\x00\x00\x02\x08'
+
+# Convert this data
+orig_data_1 = b'\x00\x00\xc4\xd6\x00\x00\xe3\x80\x00\x00\xc4\xd6\x00\x00\xf4\xd8\x00\x01\x22\x32\x00\x00\xf4\xd8\x00\x01\x22\x32\x00\x00\xe3\x80\x00\x00\xc4\xd6'
+
+# To this data (move X position 50390 to 50415), (move X position 74290 to 74265)
+new_data_1 = b'\x00\x00\xc4\xef\x00\x00\xe3\x80\x00\x00\xc4\xef\x00\x00\xf4\xd8\x00\x01\x22\x19\x00\x00\xf4\xd8\x00\x01\x22\x19\x00\x00\xe3\x80\x00\x00\xc4\xef'
+
+# This is not efficient, but only needs to be done once.
+
+with open(file_name, 'rb') as ifile:
+    data = ifile.read()
+    data = data.replace(orig_data_0, new_data_0)
+    data = data.replace(orig_data_1, new_data_1)
+
+# Write back into the same file
+with open(file_name, 'wb') as ofile:
+    ofile.write(data)
+
+print("Done!")
