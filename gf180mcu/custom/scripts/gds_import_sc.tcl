f25e1206fcc3bfc237e250aa38296cc37b116745
Tim Edwards <tim@opencircuitdesign.com>
2022-12-02 11:08:03 -0500

    Added an import script for importing standard cell GDS that removes the "flatten" option, which will cause the fill cells to get flattened out of existence.

---
+++ gf180mcu/custom/scripts/gds_import_sc.tcl
@@ -0,0 +1,5 @@
+# Set GDS import styles as needed to deal with the standard cells.
+# Set "flatten" to "false" or else the fill cells have so few
+# shapes that they get flattened out of existence.  There are no
+# instances in the standard cells that could get flattened, anyway.
+gds flatten false
