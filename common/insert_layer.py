9b4fbeb090a2e5186be4e6ebd3131129ae0e1654
Tim Edwards <tim@opencircuitdesign.com>
2023-07-30 17:23:47 -0400

    Corrected the "insert_layer.py" routine, which was missing a space and did not account for files which might have no "labels" section, and enhanced the script to accept any coordinate list with a length that is a multiple of four.  Used this script in the gf180mcu Makefile to add the isolated substrate layer into the corner I/O cell that needs it to extract correctly.

--- common/insert_layer.py
+++ common/insert_layer.py
@@ -6,6 +6,10 @@
 # The layer is added to the layout in both the mag/ (full) and maglef/
 # (abstract) directories.  Option "-maglef" or "-mag" will restrict the
 # use to only the view indicated by the option.
+#
+# Extended 7/30/2023 to allow multiple rectangles to be specified by
+# allowing any multiple of four coordinates.  So eight coordinates will
+# generate two "rect" lines, for example.
 # 
 # e.g.:
 #
@@ -23,6 +27,7 @@ def addlayer(filename, layer, geometry):
     layerrex = re.compile('<< ' + layer + ' >>')
     sectionrex = re.compile('<< ')
     labelsrex = re.compile('<< labels >>')
+    propsrex = re.compile('<< properties >>')
     endrex = re.compile('<< end >>')
 
     in_layer = False
@@ -33,19 +38,22 @@ def addlayer(filename, layer, geometry):
             if not done and not in_layer:
                 # Handle case in which layer did not already exist in file
                 lmatch = labelsrex.match(line)
+                pmatch = propsrex.match(line)
                 ematch = endrex.match(line)
-                if lmatch or ematch:
-                    print('<< ' + layer + '>>', file=ofile)
-                    print('rect ' + geometry, file=ofile)
+                if lmatch or pmatch or ematch:
+                    print('<< ' + layer + ' >>', file=ofile)
+                    for coords in zip(*[iter(geometry.split())]*4):
+                        print('rect ' + ' '.join(coords), file=ofile)
                     done = True
             
             lmatch = layerrex.match(line)
-            if lmatch:
+            if lmatch or pmatch:
                 in_layer = True
             elif in_layer:
                 smatch = sectionrex.match(line)
                 if smatch:
-                    print('rect ' + geometry, file=ofile)
+                    for coords in zip(*[iter(geometry.split())]*4):
+                        print('rect ' + ' '.join(coords), file=ofile)
                     in_layer = False
                     done = True
 
@@ -63,13 +71,13 @@ if __name__ == '__main__':
     options = []
     arguments = []
     for item in sys.argv[1:]:
-        if item.find('-', 0) == 0:
+        if item.find('-', 0) == 0 and len(item) > 1 and item[1].isalpha():
             options.append(item)
         else:
             arguments.append(item)
 
     if len(arguments) < 5:
-        print("Not enough options given to insert_layer.py.")
+        print("Not enough arguments given to insert_layer.py.")
         usage()
         sys.exit(0)
 
