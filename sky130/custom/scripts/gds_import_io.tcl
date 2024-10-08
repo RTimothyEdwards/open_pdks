c6ee9a8cc7d2214aa50469e4498ac4fb8dea5134
Tim Edwards <tim@opencircuitdesign.com>
2024-10-08 08:54:21 -0400

    Checking in some ongoing work for better handling the additional vendor cells added to the I/O library.

--- sky130/custom/scripts/gds_import_io.tcl
+++ sky130/custom/scripts/gds_import_io.tcl
@@ -17,28 +17,51 @@ gds flatglob *sky130_fd_io__amx*
 gds flatglob *sky130_fd_io__xor*
 gds flatglob *sky130_fd_io__gpiov2_amx*
 gds flatglob *sky130_fd_io__gpiov2_amux*
+gds flatglob *sky130_fd_io__gpio_ovtv2_amx*
+gds flatglob *sky130_fd_io__gpio_ovtv2_amux*
 
 ### flatten within the isolated VSSIO domain
 gds flatglob *sky130_fd_io__feas_com_pupredrvr*
 gds flatglob *sky130_fd_io__com_pupredrvr_strong_slowv2*
 gds flatglob *sky130_fd_io__com_pdpredrvr_pbiasv2*
 gds flatglob *sky130_fd_io__gpiov2_pdpredrvr_strong*
+gds flatglob *sky130_fd_io__gpio_ovtv2_pdpredrvr_strong*
+gds flatglob *sky130_fd_io__sio_pdpredrvr_strong*
 
 ### flatten in opathv2
 gds flatglob *sky130_fd_io__com_pudrvr_strong_slowv2*
 gds flatglob *sky130_fd_io__com_pdpredrvr_strong_slowv2*
 gds flatglob *sky130_fd_io__gpiov2_obpredrvr*
+gds flatglob *sky130_fd_io__gpio_ovtv2_obpredrvr*
+gds flatglob *sky130_fd_io__gpio_ovtv2_hotswap*
+gds flatglob *sky130_fd_io__sio_obpredrvr*
 gds flatglob *sky130_fd_io__hvsbt_*
 
 ### flatten in ipath
 gds flatglob *sky130_fd_io__gpiov2_ictl_logic*
+gds flatglob *sky130_fd_io__gpio_ovtv2_ictl_logic*
 
 ### avoid splitting a netlist that passes in contorted ways through the
 ### layout hierarchy
 gds flatglob *sky130_fd_io__gpio_pddrvr_strong_slowv2*
 gds flatglob *sky130_fd_io__gpiov2_pddrvr_strong*
+gds flatglob *sky130_fd_io__gpio_ovtv2_pddrvr_strong*
+gds flatglob *sky130_fd_io__sio_pddrvr_strong*
 
 ### flatten ESD devices in SIO.  Note:  HVI covering the entire device
 ### in nhvnative cell would avoid this issue.
 gds flatglob *reg_pu_nhvnative*
 gds flatglob *pu_natives*
+
+### flatten parts of pwrdetv2, where there are devices in deep nwell
+### that are in sibling cells, and resistors separated from HVI
+gds flatglob *top_pwrdetv2_res*
+gds flatglob *pwrdet_vddd*
+gds flatglob *pwrdet_vddio*
+
+### flatten parts of amuxsplitv2, where there are subcells that are
+### spread across areas inside and outside of dnwell
+gds flatglob *switch_levelshifter*
+gds flatglob *amuxsplitv2_switch_*
+gds flatglob *amuxsplitv2_hvsbt_nand*
+gds flatglob *amuxsplitv2_delay*
