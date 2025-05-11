18dbe6102a36303bb8942b490efb0f33d2815bdb
R. Timothy Edwards <tim@opencircuitdesign.com>
2025-05-11 13:57:44 -0400

    Removed the "gds unique" option from the read-in of the openRAM GDS, because (1) it became obvious that "gds unique" and "gds readonly" options cannot go together without causing major issues, and (2) the naming conflicts in openRAM that originally forced the use of "gds unique" have been resolved and it should not be necessary to use the option.  That, however, needs confirmation.

--- sky130/custom/scripts/gds_import_sram.tcl
+++ sky130/custom/scripts/gds_import_sram.tcl
@@ -1,3 +1,12 @@
 cif istyle sky130()
 # Ensure unique cell names when reading multiple GDS files
-calma unique true
+#
+# Note 1:  It is not possible to handle a hierarchy of modified
+# names when the names haven't been changed in the GDS without
+# adding a lot of bookkeeping to magic.  Therefore "gds unique"
+# should not be used in conjunction with "gds readonly".
+#
+# Note 2:  The original issue with name collisions was addressed
+# in openRAM, so this should not be needed anyway.
+#
+# calma unique true
