73324bc52afa9a9b86198ebaf61ae763c173a1a3
Iztok Jeras <iztok.jeras@gmail.com>
2026-05-26 20:02:29 +0200

    LibreLane: removing mux*_map.v files and SYNTH_MUX*_MAP variables, since they are not used (and not needed) in LibreLane

--- sky130/librelane/sky130_fd_sc_hdll/config.tcl
+++ sky130/librelane/sky130_fd_sc_hdll/config.tcl
@@ -1,8 +1,4 @@
 set current_folder [file dirname [file normalize [info script]]]
-# Technology lib
-
-# MUX2 mapping
-set ::env(SYNTH_MUX_MAP) "$::env(PDK_ROOT)/$::env(PDK)/libs.tech/librelane/$::env(STD_CELL_LIBRARY)/mux2_map.v"
 
 # Placement site for core cells
 # This can be found in the technology lef
