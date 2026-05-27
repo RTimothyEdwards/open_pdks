2a3885a803ef3f589cb08403758f17658b8c9645
Iztok Jeras <iztok.jeras@gmail.com>
2026-05-27 14:22:00 +0200

    sky130: added latches with reset
    1. Reordered ports for existing latches (`$_DLATCH_[PN]_`)
       to match the order in the cell Verilog definition.
    2. Added latches with active low reset (`\$_DLATCH_[PN]N0_`).
    3. Added cells for latches with complementary outputs,
       but they are commented out, since Yosys does not support them.
       This is done to make it explicit that additional lathes exist
       and that there are specific reasons for not mapping them.

--- sky130/librelane/sky130_fd_sc_hdll/latch_map.v
+++ sky130/librelane/sky130_fd_sc_hdll/latch_map.v
@@ -1,21 +1,28 @@
-module \$_DLATCH_P_ (input E, input D, output Q);
-  sky130_fd_sc_hdll__dlxtp_1 _TECHMAP_DLATCH_P (
-    //# {{data|Data Signals}}
-    .D(D),
-    .Q(Q),
-
-    //# {{clocks|Clocking}}
-    .GATE(E)
+// Delay latch, inverted enable, single output
+module \$_DLATCH_N_ (input E, input D, output Q);
+  sky130_fd_sc_hd__dlxtn_1 _TECHMAP_DLATCH_N (
+    .Q      (Q),
+    .D      (D),
+    .GATE_N (E)
   );
 endmodule
 
-module \$_DLATCH_N_ (input E, input D, output Q);
-  sky130_fd_sc_hdll__dlxtn_1 _TECHMAP_DLATCH_N (
-    //# {{data|Data Signals}}
-    .D(D),
-    .Q(Q),
+// Delay latch, inverted reset, non-inverted enable, single output
+module \$_DLATCH_PN0_ (input E, input R, input D, output Q);
+  sky130_fd_sc_hd__dlrtp_1 _TECHMAP_DLATCH_PN0 (
+    .Q       (Q),
+    .RESET_B (R),
+    .D       (D),
+    .GATE    (E)
+  );
+endmodule
 
-    //# {{clocks|Clocking}}
-    .GATE_N(E)
+// Delay latch, inverted reset, inverted enable, single output
+module \$_DLATCH_NN0_ (input E, input R, input D, output Q);
+  sky130_fd_sc_hd__dlrtn_1 _TECHMAP_DLATCH_NN0 (
+    .Q       (Q),
+    .RESET_B (R),
+    .D       (D),
+    .GATE_N  (E)
   );
 endmodule
