b1ead780b724863819257b8caf1569f2f13e1a17
Iztok Jeras <iztok.jeras@gmail.com>
2026-06-11 15:10:45 +0200

    gf180mcu: configuration cleanup
    * `SYNTH_CLK_DRIVING_CELL` using a clock inverter instead of a data inverter,
    * `CELL_PAD_EXCLUDE` added `fillcap_*` to cell list,
    * for `CTS_ROOT_BUFFER` use the strongest clock buffer,
    * `CTS_CLK_BUFFERS` make all buffers available for CTS instead of a subset,
    * `TRISTATE_CELLS` add tristate inverters and hold cell to list,
    * use `WELLTAP_CELL` instead of deprecated `FP_WELLTAP_CELL`,
    * use `ENDCAP_CELL` instead of deprecated `FP_ENDCAP_CELL`,
    * remove commented out `FAKEDIODE_CELL` variable,
    * use `PDN_RAIL_WIDTH` instead of deprecated `FP_PDN_RAIL_WIDTH`.

--- gf180mcu/librelane/gf180mcu_fd_sc_mcu9t5v0/config.tcl
+++ gf180mcu/librelane/gf180mcu_fd_sc_mcu9t5v0/config.tcl
@@ -5,12 +5,12 @@ set current_folder [file dirname [file normalize [info script]]]
 set ::env(PLACE_SITE) "GF018hv5v_green_sc9"
 
 # welltap and endcap cell
-set ::env(FP_WELLTAP_CELL) "$::env(STD_CELL_LIBRARY)__filltie"
-set ::env(FP_ENDCAP_CELL) "$::env(STD_CELL_LIBRARY)__endcap"
+set ::env(WELLTAP_CELL) "$::env(STD_CELL_LIBRARY)__filltie"
+set ::env(ENDCAP_CELL) "$::env(STD_CELL_LIBRARY)__endcap"
 
 # defaults (can be overridden by designs):
 set ::env(SYNTH_DRIVING_CELL) "$::env(STD_CELL_LIBRARY)__inv_1/ZN"
-set ::env(SYNTH_CLK_DRIVING_CELL) "$::env(STD_CELL_LIBRARY)__inv_4/ZN"
+set ::env(SYNTH_CLK_DRIVING_CELL) "$::env(STD_CELL_LIBRARY)__clkinv_4/ZN"
 
 # update these
 set ::env(OUTPUT_CAP_LOAD) "72.91" ; # femtofarad from pin I in liberty file
@@ -18,22 +18,20 @@ set ::env(SYNTH_BUFFER_CELL) "$::env(STD_CELL_LIBRARY)__buf_1/I/Z"
 set ::env(SYNTH_TIEHI_CELL) "$::env(STD_CELL_LIBRARY)__tieh/Z"
 set ::env(SYNTH_TIELO_CELL) "$::env(STD_CELL_LIBRARY)__tiel/ZN"
 
-# Fillcell insertion
+# Fill cell insertion
 set ::env(FILL_CELLS) "$::env(STD_CELL_LIBRARY)__fill_*"
 set ::env(DECAP_CELLS) "$::env(STD_CELL_LIBRARY)__fillcap_*"
 
 # Diode Insertion
-# A fake diode has to be created, for now don't use any strategy that uses fake diode
-# set ::env(FAKEDIODE_CELL) ""
 set ::env(DIODE_CELL) "$::env(STD_CELL_LIBRARY)__antenna/I"
 
-set ::env(CELL_PAD_EXCLUDE) "$::env(STD_CELL_LIBRARY)__filltie $::env(STD_CELL_LIBRARY)__fill_* $::env(STD_CELL_LIBRARY)__endcap"
+set ::env(CELL_PAD_EXCLUDE) "$::env(STD_CELL_LIBRARY)__filltie $::env(STD_CELL_LIBRARY)__fill_*  $::env(STD_CELL_LIBRARY)__fillcap_* $::env(STD_CELL_LIBRARY)__endcap"
 
 # TritonCTS configurations
-set ::env(CTS_ROOT_BUFFER) "$::env(STD_CELL_LIBRARY)__clkbuf_16"
-set ::env(CTS_CLK_BUFFERS) "$::env(STD_CELL_LIBRARY)__clkbuf_2 $::env(STD_CELL_LIBRARY)__clkbuf_4 $::env(STD_CELL_LIBRARY)__clkbuf_8"
+set ::env(CTS_ROOT_BUFFER) "$::env(STD_CELL_LIBRARY)__clkbuf_20"
+set ::env(CTS_CLK_BUFFERS) "$::env(STD_CELL_LIBRARY)__clkbuf_*"
 
-set ::env(FP_PDN_RAIL_WIDTH) 0.6
+set ::env(PDN_RAIL_WIDTH) 0.6
 
 # The library maximum transition is 8.9ns; setting it to lower value
 set ::env(MAX_TRANSITION_CONSTRAINT) 3
@@ -43,7 +41,7 @@ set ::env(MAX_CAPACITANCE_CONSTRAINT) 0.2
 set ::env(GPL_CELL_PADDING) {0}
 set ::env(DPL_CELL_PADDING) {0}
 
-set ::env(TRISTATE_CELLS) "$::env(STD_CELL_LIBRARY)__bufz*"
+set ::env(TRISTATE_CELLS) "$::env(STD_CELL_LIBRARY)__bufz* $::env(STD_CELL_LIBRARY)__invz* $::env(STD_CELL_LIBRARY)__hold"
 
 set ::env(SYNTH_CLOCKGATE_POSEDGE_ICG) "$::env(STD_CELL_LIBRARY)__icgtp_1/E/CLK/Q"
 set ::env(SYNTH_CLOCKGATE_NEGEDGE_ICG) "$::env(STD_CELL_LIBRARY)__icgtn_1/E/CLKN/Q"
